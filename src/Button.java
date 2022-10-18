ae0cf83437262a01977764e36e6f145cbaf5afa7
Fernanda Silva <fs.fs@hotmail.com>
2022-10-18 09:09:21 -0400

    Button customized class added

---
+++ src/Button.java
@@ -0,0 +1,131 @@
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JButton;
+
+
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
+
+
+public class Button extends JButton {
+
+	
+	public boolean isOver() {
+		return over;
+	}
+	public void setOver(boolean over) {
+		this.over = over;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+		setBackground(color);
+	}
+	public Color getColorOver() {
+		return colorOver;
+	}
+	public void setColorOver(Color colorOver) {
+		this.colorOver = colorOver;
+	}
+	public Color getColorClick() {
+		return colorClick;
+	}
+	public void setColorClick(Color colorClick) {
+		this.colorClick = colorClick;
+	}
+	public Color getBorderColor() {
+		return borderColor;
+	}
+	public void setBorderColor(Color borderColor) {
+		this.borderColor = borderColor;
+	}
+	public int getRadius() {
+		return radius;
+	}
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
+	
+	public Button() {
+		addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		//Initial COLOR
+		setColor(Color.WHITE);
+		colorOver = new Color(198,174,203);
+		colorClick = new Color(127,174,203);
+		borderColor = new Color(150,174,203);
+		setContentAreaFilled(false);
+		
+		//Mouse events
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent me) {
+				setBackground(colorOver);
+				over = true;
+				}
+			@Override
+			public void mouseExited(MouseEvent me) {
+				setBackground(color);
+				over = false;
+			}
+			@Override
+			public void mousePressed(MouseEvent me) {
+				setBackground(colorClick);
+			}
+			@Override
+			public void mouseReleased(MouseEvent me) {
+				if(over) {
+					setBackground(colorOver);
+				}else {
+					setBackground(color);
+				}
+			}
+			
+		});
+		
+	}
+
+	private boolean over;
+	private Color color;
+	private Color colorOver;
+	private Color colorClick;
+	private Color borderColor;
+	private int radius = 0;
+	/**
+	 * @wbp.nonvisual location=111,139
+	 */
+	private final JButton button = new JButton("New button");
+	
+	@Override
+	protected void paintComponent(Graphics graphics) {
+		Graphics2D g2=(Graphics2D)graphics;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		
+		//Paint border
+		g2.setColor(borderColor);
+		g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+		g2.setColor(getBackground());
+		//Border 2px
+		g2.fillRoundRect(1, 1, getWidth()-2, getHeight()-2, radius, radius);
+		super.paintComponent(graphics);
+		
+	}
+	
+	
+	
+}
--- src/TextEditor.java
+++ src/TextEditor.java
@@ -27,6 +27,10 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Point;
+import java.awt.ComponentOrientation;
 
 public class TextEditor extends JFrame implements ActionListener {
 	
@@ -34,7 +38,7 @@ public class TextEditor extends JFrame implements ActionListener {
 	JScrollPane scrollPane;
 	JLabel fontLabel;
 	JSpinner fontSizeSpinner; //for the user be able to change the font size
-	JButton fontColorButton; //user will change color for the font
+	Button fontColorButton; //user will change color for the font
 	JComboBox fontBox; //user will be able to change fonts
 	
 	//menu bar var
@@ -53,26 +57,39 @@ public class TextEditor extends JFrame implements ActionListener {
 	
 	
 	TextEditor(){
+		getContentPane().setBackground(new Color(199, 199, 216));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //click x on the corner to exit
 		this.setTitle("FEFS Text Editor Project!");
-		this.setSize(500, 500); //size of screen
-		this.setLayout(new FlowLayout()); //can set to null and manually place all components
+		this.setSize(500, 500);
 		this.setLocationRelativeTo(null); //this will set the window to be at the middle of the screen
 		
 		
 		textArea = new JTextArea();
-		//textArea.setPreferredSize(new Dimension(450,450)); //no scroll
+		textArea.setBorder(null);
+		textArea.setPreferredSize(new Dimension(450,450)); //no scroll
 		textArea.setLineWrap(true); //this will let the text continue in the next line
 		textArea.setWrapStyleWord(true);
-		textArea.setFont(new Font("Comic Sans MS",Font.PLAIN,20));//how to import font? 6:39
+		textArea.setFont(new Font("Comic Sans MS",Font.PLAIN,20));
 		
 		scrollPane = new JScrollPane(textArea);
-		scrollPane.setPreferredSize(new Dimension(450,450)); //this add the paper window with the scroll.
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS); //this make the scrollbar always visible
+		scrollPane.setBackground(new Color(255, 255, 234));
+		scrollPane.setBorder(null);
+		scrollPane.setBounds(18, 36, 450, 450);
+		scrollPane.setPreferredSize(new Dimension(450,450));
 		
 		fontLabel = new JLabel("Size:");
+		fontLabel.setBackground(new Color(199, 199, 216));
+		fontLabel.setFont(new Font("MoeumT R", Font.BOLD, 15));
+		fontLabel.setBounds(179, 13, 40, 16);
+		fontLabel.setForeground(new Color(102, 102, 147));
 		
 		fontSizeSpinner = new JSpinner();
+		fontSizeSpinner.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		fontSizeSpinner.setForeground(new Color(102, 102, 147));
+		fontSizeSpinner.setFont(new Font("MoeumT R", Font.BOLD, 12));
+		fontSizeSpinner.setBackground(new Color(199, 199, 216));
+		fontSizeSpinner.setBorder(null);
+		fontSizeSpinner.setBounds(217, 12, 40, 16);
 		fontSizeSpinner.setPreferredSize(new Dimension(50,25));
 		fontSizeSpinner.setValue(20);
 		fontSizeSpinner.addChangeListener(new ChangeListener() {
@@ -85,7 +102,18 @@ public class TextEditor extends JFrame implements ActionListener {
 			}
 		});
 		
-		fontColorButton = new JButton("Color");
+		fontColorButton = new Button();
+		fontColorButton.setBorderPainted(false);
+		fontColorButton.setForeground(new Color(102, 102, 147));
+		fontColorButton.setFont(new Font("MoeumT R", Font.BOLD, 15));
+		fontColorButton.setBounds(18, 6, 101, 26);
+		fontColorButton.setColorOver(new Color(167, 167, 211));
+		fontColorButton.setColorClick(new Color(205, 205, 231));
+		fontColorButton.setColor(new Color(255, 255, 255));
+		fontColorButton.setBorderColor(new Color(102, 102, 147));
+		fontColorButton.setBackground(new Color(199, 199, 216));
+		fontColorButton.setRadius(25);
+		fontColorButton.setText("Color");
 		fontColorButton.addActionListener(this);
 	
 		
@@ -95,22 +123,97 @@ public class TextEditor extends JFrame implements ActionListener {
 		
 		//JComboBox let you add options to the stray combo box by passing in an array to this constructor.
 		fontBox = new JComboBox(fonts);
+		fontBox.setBackground(new Color(199, 199, 216));
+		fontBox.setFont(new Font("MoeumT R", Font.BOLD, 12));
+		fontBox.setForeground(new Color(102, 102, 147));
+		fontBox.setBorder(null);
+		fontBox.setBounds(317, 9, 151, 20);
 		fontBox.addActionListener(this);
 		fontBox.setSelectedItem("Comic Sans MS"); //set initial font
 		
 		//Menubar
 		menuBar = new JMenuBar();
+		menuBar.setBounds(new Rectangle(0, 0, 0, 20));
+		menuBar.setMargin(new Insets(400, 0, 400, 0));
+		menuBar.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		menuBar.setForeground(new Color(255, 255, 255));
+		menuBar.setBorderPainted(false);
+		menuBar.setBackground(new Color(102, 102, 147));
+		menuBar.setBorder(null);
 		fileMenu = new JMenu("File");
+		fileMenu.setMargin(new Insets(10, 2, 10, 2));
+		fileMenu.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		fileMenu.setForeground(new Color(255, 255, 255));
+		fileMenu.setBorder(null);
+		fileMenu.setBackground(new Color(102, 102, 147));
 		editMenu = new JMenu("Edit");
+		editMenu.setMargin(new Insets(10, 2, 10, 2));
+		editMenu.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		editMenu.setForeground(new Color(255, 255, 255));
+		editMenu.setBorder(null);
+		editMenu.setBackground(new Color(102, 102, 147));
 		aboutMenu = new JMenu("About");
+		aboutMenu.setMargin(new Insets(10, 2, 10, 2));
+		aboutMenu.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		aboutMenu.setForeground(new Color(255, 255, 255));
+		aboutMenu.setBorder(null);
+		aboutMenu.setBackground(new Color(102, 102, 147));
 		newItem = new JMenuItem("New");
+		newItem.setMargin(new Insets(10, 2, 10, 2));
+		newItem.setBorderPainted(false);
+		newItem.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		newItem.setForeground(new Color(255, 255, 255));
+		newItem.setBorder(null);
+		newItem.setBackground(new Color(102, 102, 147));
 		openItem = new JMenuItem("Open");
+		openItem.setMargin(new Insets(10, 2, 10, 2));
+		openItem.setBorderPainted(false);
+		openItem.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		openItem.setForeground(new Color(255, 255, 255));
+		openItem.setBorder(null);
+		openItem.setBackground(new Color(102, 102, 147));
 		saveItem = new JMenuItem("Save");
+		saveItem.setMargin(new Insets(10, 2, 10, 2));
+		saveItem.setBorderPainted(false);
+		saveItem.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		saveItem.setForeground(new Color(255, 255, 255));
+		saveItem.setBorder(null);
+		saveItem.setBackground(new Color(102, 102, 147));
 		exitItem = new JMenuItem("Exit");
+		exitItem.setMargin(new Insets(10, 2, 10, 2));
+		exitItem.setBorderPainted(false);
+		exitItem.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		exitItem.setForeground(new Color(255, 255, 255));
+		exitItem.setBorder(null);
+		exitItem.setBackground(new Color(102, 102, 147));
 		cutItem = new JMenuItem("Cut");
+		cutItem.setMargin(new Insets(10, 2, 10, 2));
+		cutItem.setBorderPainted(false);
+		cutItem.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		cutItem.setForeground(new Color(255, 255, 255));
+		cutItem.setBorder(null);
+		cutItem.setBackground(new Color(102, 102, 147));
 		copyItem = new JMenuItem("Copy");
+		copyItem.setMargin(new Insets(10, 2, 10, 2));
+		copyItem.setBorderPainted(false);
+		copyItem.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		copyItem.setForeground(new Color(255, 255, 255));
+		copyItem.setBorder(null);
+		copyItem.setBackground(new Color(102, 102, 147));
 		pasteItem = new JMenuItem("Paste");
+		pasteItem.setMargin(new Insets(10, 2, 10, 2));
+		pasteItem.setBorderPainted(false);
+		pasteItem.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		pasteItem.setForeground(new Color(255, 255, 255));
+		pasteItem.setBorder(null);
+		pasteItem.setBackground(new Color(102, 102, 147));
 		infoItem = new JMenuItem("Info");
+		infoItem.setMargin(new Insets(10, 2, 10, 2));
+		infoItem.setBorderPainted(false);
+		infoItem.setFont(new Font("MoeumT R", Font.PLAIN, 15));
+		infoItem.setForeground(new Color(255, 255, 255));
+		infoItem.setBorder(null);
+		infoItem.setBackground(new Color(102, 102, 147));
 		
 		//adding action listeners to the items on file menu
 		newItem.addActionListener(this);
@@ -147,11 +250,18 @@ public class TextEditor extends JFrame implements ActionListener {
 		
 		//Adding to the frame
 		this.setJMenuBar(menuBar);
-		this.add(fontLabel);
-		this.add(fontSizeSpinner);
-		this.add(fontColorButton);
-		this.add(fontBox);
-		this.add(scrollPane);
+		getContentPane().setLayout(null);
+		getContentPane().add(fontLabel);
+		getContentPane().add(fontSizeSpinner);
+		getContentPane().add(fontColorButton);
+		getContentPane().add(fontBox);
+		getContentPane().add(scrollPane);
+		
+		JLabel lblFont = new JLabel("Font:");
+		lblFont.setFont(new Font("MoeumT R", Font.BOLD, 15));
+		lblFont.setForeground(new Color(102, 102, 147));
+		lblFont.setBounds(279, 13, 40, 16);
+		getContentPane().add(lblFont);
 		this.setVisible(true);
 	}
 	
